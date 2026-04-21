81a6e73e43f56ca68d95ece83b510c6a5682116d
yahirtellez-desing <yahirtellez_@hotmail.com>
2026-04-21 17:45:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cors({
     'http://127.0.0.1:5500',
     'http://localhost:3001',
     // Agrega tu dominio de producción aquí:
-    // 'https://tu-dominio.com',
+    'https://qualamx.vercel.app/',
   ],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
